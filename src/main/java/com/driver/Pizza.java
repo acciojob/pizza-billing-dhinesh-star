61828d5b97abaefe8363ed1ac1e67967ef250af8
dhinesh-star <dhineshmanickam2001@gmail.com>
2023-12-18 23:55:48 +0530

    Submission 5

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,13 +8,9 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    private List<String> billList=new ArrayList<>();
     private Boolean isCheeseAdded;
     private Boolean isToppingAdded;
     private Boolean isPaperBagAdded;
-    /*toppingAddedIdx is to keep track of the topping added
-    index extra cheese above it*/
-    private Integer toppingAddedIdx=-1;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         isCheeseAdded=false;
@@ -24,11 +20,9 @@ public class Pizza {
         // your code goes here
         if(isVeg==true){
             this.price = 300;
-            billList.add("Base Price Of The Pizza: 300");
         }
         else{
             this.price=400;
-            billList.add("Base Price Of The Pizza: 400");
         }
     }
 
@@ -41,12 +35,6 @@ public class Pizza {
         if(isCheeseAdded==false){
             isCheeseAdded=true;
             price += 80;
-            if(toppingAddedIdx==-1) {
-                billList.add("Extra Cheese Added: 80");
-            }
-            else{
-                billList.add(toppingAddedIdx,"Extra Cheese Added: 80");
-            }
         }
     }
 
@@ -54,14 +42,11 @@ public class Pizza {
         // your code goes here
         if(isToppingAdded==false){
             isToppingAdded=true;
-            toppingAddedIdx=billList.size();
             if(isVeg==true){
                 price += 70;
-                billList.add("Extra Toppings Added: 70");
             }
             else{
                 price += 120;
-                billList.add("Extra Toppings Added: 120");
             }
         }
     }
@@ -71,19 +56,18 @@ public class Pizza {
         if(isPaperBagAdded==false){
             this.isPaperBagAdded=true;
             price += 20;
-            billList.add("Paperbag Added: 20");
         }
     }
 
     public String getBill(){
         // your code goes here
         StringBuilder sb=new StringBuilder();
-        sb.append("Base Price Of The Pizza: ").append(isVeg?300:400).append("\n");
+        sb.append("Base Price Of The Pizza: ").append(isVeg ? 300 : 400).append("\n");
         if(isCheeseAdded==true){
             sb.append("Extra Cheese Added: 80").append("\n");
         }
         if(isToppingAdded==true){
-            sb.append("Extra Toppings Added: ").append(isVeg?70:120).append("\n");
+            sb.append("Extra Toppings Added: ").append(isVeg ?70 : 120).append("\n");
         }
         if(isPaperBagAdded==true){
             sb.append("Paperbag Added: 20").append("\n");
